1ec584f4a6b17c24d5fc335284bea4aa8e809761
SukiTKChan <sukitkchan@gmail.com>
2018-04-26 23:30:39 +0100

    changes to EchoClient1Test

--- src/test/java/EchoClient1Test.java
+++ src/test/java/EchoClient1Test.java
@@ -1,32 +1,44 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
+
+
 public class EchoClient1Test
-{    static final String endMessage = "";
+{   private final ByteArrayOutputStream outputName = new ByteArrayOutputStream();
+    //private final ByteArrayOutputStream outputPassword = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outputName));
+        //System.setOut(new PrintStream(outputPassword));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
 
     @Test
     public void main()throws Exception
     {
+
         try
         {
             String name = "suki";
-            String password = "123456";
+            System.out.print("suki");
+            assertEquals(name,outputName.toString());
 
-
-            assertEquals(name,"suki");
-            assertEquals(password,"123456");
         }
         catch (Exception e)
         {
             Assert.fail("Exception " + e);
         }
-
-
 
     }
 }
